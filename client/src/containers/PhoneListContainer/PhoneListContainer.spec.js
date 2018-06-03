9c21cb01c8a4b061573ce755efd9d6b1efdaa350
gabriel <ggarciaseco@gmail.com>
2018-06-03 08:32:16 +0200

    test: Add spec for the PhoneListContainer

--- client/src/containers/PhoneListContainer/PhoneListContainer.spec.js
+++ client/src/containers/PhoneListContainer/PhoneListContainer.spec.js
@@ -1,15 +1,48 @@
-/* import React from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
-import { PhotoListContainerTesting } from './PhotoListContainer';
+import {
+  PhotoListContainerTesting,
+  mapStateToProps
+} from './PhoneListContainer';
+import { phonesFake } from 'fakes/phone';
+import { PhonesList, Spinner } from 'components';
 
-describe('PhotoListContainer suite', () => {
-  it('renders PhotoListContainer without any state injected', () => {
-    const component = shallow(<PhotoListContainerTesting />);
-    expect(component).toBeDefined();
+const dispatch = jest.fn();
+
+describe('PhoneListContainer', () => {
+  it('renders App with the loading state', () => {
+    const wrapper = shallow(
+      <PhotoListContainerTesting dispatch={dispatch} isLoading />
+    );
+
+    expect(wrapper).toBeDefined();
+    expect(wrapper.find(Spinner).length).toBe(1);
+    expect(wrapper.find(PhonesList).length).toBe(0);
   });
-});
- */
 
-describe('Todo PhoneListContainer', () => {
-  it('should todo', () => {});
+  it('renders App with the loading state', () => {
+    const wrapper = shallow(
+      <PhotoListContainerTesting dispatch={dispatch} isLoaded />
+    );
+
+    expect(wrapper).toBeDefined();
+    expect(wrapper.find(Spinner).length).toBe(0);
+    expect(wrapper.find(PhonesList).length).toBe(1);
+    expect(dispatch).toHaveBeenCalled();
+  });
+
+  it('should test mapStateToProps', () => {
+    const state = {
+      phones: {
+        phones: phonesFake,
+        isLoading: false,
+        isLoaded: false
+      }
+    };
+    expect(mapStateToProps(state)).toEqual({
+      phones: state.phones.phones,
+      isLoading: state.phones.isLoading,
+      isLoaded: state.phones.isLoaded
+    });
+  });
 });
