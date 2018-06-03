5f5ffab0ed344b4f2af525309fb73b684ff069de
gabriel <ggarciaseco@gmail.com>
2018-06-03 07:57:19 +0200

    test: Add specs for phoneList and phoneListItem components

--- client/src/components/PhonesList/PhonesList.spec.js
+++ client/src/components/PhonesList/PhonesList.spec.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import PhoneList from './PhonesList';
+import { phonesFake } from 'fakes/phone';
 
 describe('PhoneList suite', () => {
   it('should render the ProductsList component', () => {
-    const component = shallow(<PhoneList phones={[]} />);
+    const jsxComponent = (
+      <Router>
+        <PhoneList phones={phonesFake} />
+      </Router>
+    );
+    const component = shallow(<PhoneList phones={phonesFake} />);
+    const tree = renderer.create(jsxComponent).toJSON();
     expect(component).toBeDefined();
+    expect(tree).toMatchSnapshot();
   });
 });
--- client/src/components/PhonesListItem/PhoneListItem.spec.js
+++ client/src/components/PhonesListItem/PhoneListItem.spec.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import PhoneListItem from './PhoneListItem';
+import { phonesFake } from 'fakes/phone';
 
 describe('PhoneListItem suite', () => {
   it('should render the ProductsListItem component', () => {
-    const component = shallow(<PhoneListItem phone={{}} />);
+    const jsxComponent = (
+      <Router>
+        <PhoneListItem phone={phonesFake[0]} />
+      </Router>
+    );
+    const component = shallow(jsxComponent);
+    const tree = renderer.create(jsxComponent).toJSON();
     expect(component).toBeDefined();
+    expect(tree).toMatchSnapshot();
   });
 });
